71bdae87951b22120154380efb01af3bd4b0e8bc
gabrieladvent <bie.ritan112@gmail.com>
2023-02-21 12:37:17 +0700

    first commit

---
+++ app/src/main/java/com/example/miniprojekrawlabs/screen/TampilanAwal.kt
@@ -0,0 +1,12 @@
+package com.example.miniprojekrawlabs.screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.miniprojekrawlabs.R
+
+class TampilanAwal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tampilan_awal)
+    }
+}
---
+++ app/src/main/java/com/example/miniprojekrawlabs/screen/login/TampilanLogin.kt
@@ -0,0 +1,12 @@
+package com.example.miniprojekrawlabs.screen.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.miniprojekrawlabs.R
+
+class TampilanLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tampilan_login)
+    }
+}
